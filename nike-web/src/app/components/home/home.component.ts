f291d130c55d4c885d19aa4274bc7eb043de6833
duylee23 <coobedangyeuu@gmail.com>
2025-02-27 15:28:15 +0700

    setting slider,header

--- nike-web/src/app/components/home/home.component.ts
+++ nike-web/src/app/components/home/home.component.ts
@@ -5,42 +5,20 @@ import { Navigation, Pagination } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+import { SliderComponent } from '../../shared/slider/slider.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [SliderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 
-export class HomeComponent implements AfterViewInit  {
+export class HomeComponent   {
 
   constructor() { }
-  ngAfterViewInit(): void {
-    const mySwiper = new Swiper('.swiper-container', {
-      // Optional parameters
-      direction: 'horizontal',
-      loop: true,
-
-      // If you need pagination
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-
-      // Navigation arrows
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-
-      // Autoplay
-      autoplay: {
-        delay: 3000, // 3 seconds
-      },
-    });
-  }
+ 
 
 }
---
+++ nike-web/src/app/shared/slider/slider.component.ts
@@ -0,0 +1,34 @@
+import { AfterViewInit, Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { register } from 'swiper/element/bundle';
+@Component({
+  selector: 'app-slider',
+  standalone: true,
+  imports: [],
+  templateUrl: './slider.component.html',
+  styleUrl: './slider.component.css',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class SliderComponent implements AfterViewInit {
+  constructor() { 
+    register();
+
+  }
+  private swiperEl?: any;
+  autoplayConfig = {
+    delay: 3000, 
+    disableOnInteraction: false
+  };
+  ngAfterViewInit(): void {
+    this.swiperEl = document.querySelector('swiper-container');
+    if(this.swiperEl) {
+      const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+          if(entry.isIntersecting){
+            this.swiperEl.swiper.autoplay.start();
+          }
+        })
+      })
+      observer.observe(this.swiperEl)
+    }
+  }
+}
