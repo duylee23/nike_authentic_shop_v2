3de16f05a6ca3fca47e90709a2e14ab0fd7e1e83
duylee23 <coobedangyeuu@gmail.com>
2025-03-18 22:51:52 +0700

    add route

--- nike-web/src/app/app.routes.ts
+++ nike-web/src/app/app.routes.ts
@@ -3,11 +3,12 @@ import { HomeComponent } from './components/home/home.component';
 import { DetailsComponent } from './components/details/details.component';
 import { AboutComponent } from './components/about/about.component';
 import { ContactComponent } from './components/contact/contact.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 export const routes: Routes = [
     {path:'', component: HomeComponent},
     {path:'details', component: DetailsComponent},
     {path:'about', component: AboutComponent},
     {path:'contact', component: ContactComponent},
-    {path:'**', redirectTo: ''}
+    {path:'**', component: NotFoundComponent }
 ];
